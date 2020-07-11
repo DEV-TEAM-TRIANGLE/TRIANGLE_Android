cb03e9f28471ca46be0362c9765d2424d442a9ec
ChoHadam <hadamcho97@gmail.com>
2020-07-11 18:24:40 +0900

    Writing.js motto(좌우명) 추가(서버 연동전)

--- triangle/Screens/Writing.js
+++ triangle/Screens/Writing.js
@@ -4,7 +4,7 @@ import {
     Text,
     StyleSheet,
     Dimensions,
-    Button
+    Button,
     } from 'react-native';
 
 const {width, height} = Dimensions.get("window");
@@ -17,13 +17,16 @@ class Writing extends Component{
                     <View>
                     </View>
                     <View style={style.actions}>
-                        <View >
-                            <Button onPress = {() => alert('작성중인 회고가 임시 저장되었습니다.')} title='저장' color='#1e22af'/>
+                        <View>
+                            <Button onPress = {() => alert('작성중인 회고가 임시 저장되었습니다.')} title='저장' color='#1e22af' />
                         </View>
                         <View>
                             <Button onPress = {() => alert('회고 작성이 완료되었습니다.')} title='완료' color='#1e22af' />
                         </View>
                     </View>
+                </View>
+                <View style={style.motto}>
+                    <Text style={style.mottoText}>사람의 마음을 울리는 컨텐츠 크리에이터가 되자 ✨ </Text>
                 </View>
             </View>
         );
@@ -35,15 +38,37 @@ const style = StyleSheet.create({
     container: {
         flex: 1, //flex item이 빈 공간을 채우도록 한다.
         backgroundColor: 'white',
-
     },
     bar: {
-        backgroundColor: 'white',
         marginTop : 24,
+        height : 35,
     },
     actions: {
         flexDirection : 'row',
-        justifyContent: "flex-end"
+        justifyContent: "flex-end",
+    },
+    motto: {
+        height : 36,
+        justifyContent: "center",
+        backgroundColor : "white", //배경색 설정 안해주면 뷰 내부 텍스트에 그림자가 생기므로, 뷰 그림자를 만드려면 배경색을 지적해줘야한다.
+        ...Platform.select({
+            ios: {
+                shadowColor: "black",
+                shadowOpacity: 0.08,
+                shadowRadius: 5,
+                shadowOffset: {
+                    height: 6
+                }
+            },
+            android: {
+                elevation: 5,
+            }
+        })
+    },
+    mottoText: {
+        paddingLeft : 15,
+        fontSize : 14,
+        fontWeight : 'bold',
     }
 
 })
